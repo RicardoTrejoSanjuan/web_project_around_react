e5540e54399fd8e5ddaa666dc539bd16a37af1a2
RicardoTrejoSanjuan <trejosanjuanricardo@gmail.com>
2026-07-07 12:59:56 -0700

    feat: add CurrentUserContext consumption and mock popup component to Main layout

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,8 +1,15 @@
 import type { JSX } from "react/jsx-runtime";
 import Profile from "@/components/Profile/Profile";
 import CardList from "@/components/Main/components/Card/CardList";
+import CurrentUserContext from "@/contexts/CurrentUserContext";
+import Popup from "@/components/Main/components/popup/Popup";
+import { useContext } from "react";
 
 const Main = (): JSX.Element => {
+  const currentUser = useContext(CurrentUserContext);
+  console.log("currentUser: ", currentUser);
+  const shouldShowPopup = false;
+
   return (
     <main className="content">
       {/* PROFILE SECTION */}
@@ -12,6 +19,11 @@ const Main = (): JSX.Element => {
       <section className="cards page__section">
         <CardList />
       </section>
+      {shouldShowPopup && (
+        <Popup onClose={() => {}} title="soy un titulo">
+          <p>Soy un popup mock para las pruebas automaticas</p>
+        </Popup>
+      )}
     </main>
   );
 };
